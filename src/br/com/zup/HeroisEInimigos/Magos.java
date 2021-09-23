72129c068b4cd2d7c7f98fe464cc094c6e3b6080
Let Marçal <leticiamarcalmatias@gmail.com>
2021-09-23 12:28:15 -0300

    Reproduzindo o mago do diagrama de classes

---
+++ src/br/com/zup/HeroisEInimigos/Magos.java
@@ -0,0 +1,35 @@
+package br.com.zup.HeroisEInimigos;
+
+/*
+Primeiro nome da Classe;
+Atributos da classe informando o modificador de acesso;
+métodos da classe tbm com os modficadores de acesso
++ public
+- private
+# protected
+~ default
+ */
+
+public class Magos extends Heroi {
+    private double poderMagico;
+
+    //Método construtor do Mago
+    public Magos(String nome, double vida, double poderMagico) {
+        super(nome, vida);
+        this.poderMagico = poderMagico;
+    }
+
+    //Getters e Setters
+    public double getPoderMagico() {
+        return poderMagico;
+    }
+
+    public void setPoderMagico(double poderMagico) {
+        this.poderMagico = poderMagico;
+    }
+
+    //Método aplicar magia
+    public double aplicarMagia(){
+        return poderMagico;
+    }
+}
