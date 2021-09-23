99b034abbcbb6d43af5052382652cd4bad04fef7
Let Marçal <leticiamarcalmatias@gmail.com>
2021-09-23 13:34:34 -0300

    Reproduzindo inimigos rasteiros

---
+++ src/br/com/zup/HeroisEInimigos/Rasteiros.java
@@ -0,0 +1,25 @@
+package br.com.zup.HeroisEInimigos;
+
+public class Rasteiros extends Inimigos {
+    private double danoEspinho;
+
+    //Método construtor
+    public Rasteiros(String nome, double vida, double danoEspinho) {
+        super(nome, vida);
+        this.danoEspinho = danoEspinho;
+    }
+
+    //Getters and Setters
+    public double getDanoEspinho() {
+        return danoEspinho;
+    }
+
+    public void setDanoEspinho(double danoEspinho) {
+        this.danoEspinho = danoEspinho;
+    }
+
+    //Método aplicar dano
+    private double aplicarDano(){
+        return danoEspinho;
+    }
+}
