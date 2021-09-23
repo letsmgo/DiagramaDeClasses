d40ad484d70ebdea1e89f69820d6879a65266e6c
Let Marçal <leticiamarcalmatias@gmail.com>
2021-09-23 13:29:36 -0300

    Reproduzindo os Inimigos

---
+++ src/br/com/zup/HeroisEInimigos/Inimigos.java
@@ -0,0 +1,47 @@
+package br.com.zup.HeroisEInimigos;
+
+public class Inimigos {
+    private String nome;
+    private double vida;
+
+    //Método construtor
+    public Inimigos(String nome, double vida){
+        this.nome = nome;
+        this.vida = vida;
+    }
+
+    //Getters and Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVida() {
+        return vida;
+    }
+
+    public void setVida(double vida) {
+        this.vida = vida;
+    }
+
+
+    //Método do diagrama trocar nome
+    public void trocarNome(String nome) {
+        String novoNome = "";
+        setNome(novoNome);
+    }
+
+    //Método do diagrama receber dano
+    public void receberDano(double dano) {
+        double danoRecebido = getVida() - dano;
+        this.setVida(danoRecebido);
+    }
+
+    //Método do diagrama exibir vida
+    public double exibirVida(){
+        return getVida();
+    }
+}
